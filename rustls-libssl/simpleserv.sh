def66087acfeee21c718222de2e529ad72ddfcb5
Daniel McCarney <daniel@binaryparadox.net>
2024-04-23 09:52:07 -0400

    ci: verify ASAN presence/absence as appropriate

--- rustls-libssl/simpleserv.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-openssl s_server \
-  -cert test-ca/rsa/end.cert \
-  -cert_chain test-ca/rsa/inter.cert \
-  -key test-ca/rsa/end.key \
-  -alpn "hello,world" \
-  -accept localhost:4443 \
-  -rev

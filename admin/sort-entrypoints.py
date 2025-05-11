fc6aab43615380eb29a55a3091976d666b68e250
Joe Birr-Pixton <jpixton@gmail.com>
2025-05-11 08:25:34 +0100

    sort-entrypoints.py: format

--- admin/sort-entrypoints.py
+++ admin/sort-entrypoints.py
@@ -6,23 +6,29 @@ import difflib
 entries = []
 in_entries = False
 
-location = path.join(path.dirname(__file__), '../build.rs')
+location = path.join(path.dirname(__file__), "../build.rs")
 
 for line in open(location):
     if in_entries:
-        if '];' in line:
+        if "];" in line:
             in_entries = False
             continue
 
         entries.append(line)
 
-    if 'const ENTRYPOINTS:' in line:
+    if "const ENTRYPOINTS:" in line:
         in_entries = True
 
 assert len(entries) != 0
 sorted_entries = list(sorted(entries, key=lambda s: s.lower()))
 
 if entries != sorted_entries:
-    print('!!! ENTRYPOINTS is not sorted')
-    print(''.join(difflib.context_diff(entries, sorted_entries, fromfile='build.rs', tofile='build.rs')))
+    print("!!! ENTRYPOINTS is not sorted")
+    print(
+        "".join(
+            difflib.context_diff(
+                entries, sorted_entries, fromfile="build.rs", tofile="build.rs"
+            )
+        )
+    )
     exit(1)
